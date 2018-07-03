8e5e8a22f1ca90507d898b9b9f77c8b7f5b77a1c
atlassubbed <atlassubbed@gmail.com>
2018-07-03 18:29:40 -0400

    implements transform stream interface

--- src/HtmlParser.js
+++ src/HtmlParser.js
@@ -1,1 +1,15 @@
-console.log("Hello from atlas-html-stream!")
+const SeqMatcher = require("atlas-seq-matcher");
+const { Transform } = require("stream");
+const { TEXT, NODE, NAME, KEY, VALUE, SCRIPT, STYLE } = require("./states");
+
+module.exports = class HtmlParser extends Transform {
+  constructor(){
+    super({readableObjectMode: true})
+  }
+  _transform(chunk, encoding, done){
+    done(null)
+  }
+  _flush(done){
+    done(null)
+  }
+}
---
+++ src/states.js
@@ -0,0 +1,9 @@
+module.exports = {
+  TEXT:0, 
+  NODE:1, 
+  NAME:2, 
+  KEY:3, 
+  VALUE:4, 
+  SCRIPT:5, 
+  STYLE: 6
+}
